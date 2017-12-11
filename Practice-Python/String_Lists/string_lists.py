4fbe2c100b47fcbe61928936119eb42c3dcb0c2b
Ivan Leo <ivanleo97@gmail.com>
2017-12-11 16:49:35 +0800

    Update string_lists.py

--- Practice-Python/String_Lists/string_lists.py
+++ Practice-Python/String_Lists/string_lists.py
@@ -2,7 +2,7 @@ string = input("please input the string to be compared: ")
 
 print("The Original string is: " + string)
 
-if (string.replace("\\s","") == (string.replace("\\s","")[::-1])):
-  print("This is a palindrome! When reversed, it forms "+ string[::-1])
+if (string.replace("\\s","").lower() == (string.replace("\\s","").lower()[::-1])):
+  print("This is a palindrome! When reversed, it forms "+ string.replace("\\s","").lower()[::-1])
 else:
   print("this is not a palindrome!")
